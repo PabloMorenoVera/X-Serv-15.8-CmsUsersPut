878cde1bd2675c544a17cacb7f9cd979848a67f5
PabloMorenoVera <pablomvera14@gmail.com>
2018-04-17 09:44:15 +0200

    Fixed app name in settings installed apps

--- myproject/settings.py
+++ myproject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'UsersPut',
+    'cms_users_put',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
